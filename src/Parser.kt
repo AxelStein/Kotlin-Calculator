477a215c5b8b7fa7209cbe5c5ee02dec7c404c3b
alex <diefaust1993@gmail.com>
2022-04-01 14:24:03 +0300

    Implement float calculation

--- src/Parser.kt
+++ src/Parser.kt
@@ -1,5 +1,3 @@
-import com.sun.org.apache.xpath.internal.operations.Bool
-
 class Parser(private val lexer: Lexer) {
     private val priorityMap = mapOf('+' to 1,
             '-' to 1,
@@ -7,11 +5,11 @@ class Parser(private val lexer: Lexer) {
             '/' to 2
     )
 
-    fun parse(): Int {
+    fun parse(): Float {
         return parseExpression(0)
     }
 
-    private fun parseExpression(priority: Int): Int {
+    private fun parseExpression(priority: Int): Float {
         var result = parseItem(lexer.nextToken())
         while (getSymbolPriority(lexer.peekNextToken()) > priority) {
             result = parseTerm(result, lexer.nextToken())
@@ -19,7 +17,7 @@ class Parser(private val lexer: Lexer) {
         return result
     }
 
-    private fun parseTerm(left: Int, token: Token): Int {
+    private fun parseTerm(left: Float, token: Token): Float {
         val priority = getSymbolPriority(token)
         val right = parseExpression(priority)
         token as TokenSymbol
@@ -32,21 +30,33 @@ class Parser(private val lexer: Lexer) {
         }
     }
 
-    private fun parseItem(token: Token): Int {
+    private fun parseItem(token: Token): Float {
         if (token is TokenSymbol) {
             if (token.value == '-') {
                 val nextToken = lexer.peekNextToken()
-                if (nextToken is TokenInt) {
+                if (nextToken.isInt() || nextToken.isFloat()) {
                     lexer.nextToken()
-                    return nextToken.value * -1
+                    if (nextToken is TokenInt) {
+                        return nextToken.value.toFloat() * -1f
+                    }
+                    if (nextToken is TokenFloat) {
+                        return nextToken.value * -1f
+                    }
                 }
             }
         }
         if (token is TokenInt) {
+            return token.value.toFloat()
+        }
+        if (token is TokenFloat) {
             return token.value
         }
-        return 0
+        return 0f
     }
+
+    private fun Token.isInt() = this is TokenInt
+
+    private fun Token.isFloat() = this is TokenFloat
 
     private fun getSymbolPriority(token: Token): Int {
         if (token is TokenSymbol) {
